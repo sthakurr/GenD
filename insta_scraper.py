3a7af2d96443c08c0cb9749a7c234539121ef9c9
Saumya Thakur <thakursaumya01@gmail.com>
2020-04-14 13:41:20 +0530

    Initial

---
+++ insta_scraper.py
@@ -0,0 +1,95 @@
+from random import choice
+import json
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+
+_user_agents = [
+    'Chrome/79.0.3945.88'
+]
+
+
+class InstagramScraper:
+
+    def __init__(self, user_agents=None, proxy=None):
+        self.user_agents = user_agents
+        self.proxy = proxy
+
+    def __random_agent(self):
+        if self.user_agents and isinstance(self.user_agents, list):
+            return choice(self.user_agents)
+        return choice(_user_agents)
+
+    def __request_url(self, url):
+        try:
+            response = requests.get(url, headers={'User-Agent': self.__random_agent()}, proxies={'http': self.proxy,
+                                                                                                 'https': self.proxy})
+            response.raise_for_status()
+        except requests.HTTPError:
+            raise requests.HTTPError('Received non 200 status code from Instagram')
+        except requests.RequestException:
+            raise requests.RequestException
+        else:
+            return response.text
+
+    @staticmethod
+    def extract_json_data(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        body = soup.find('body')
+        script_tag = body.find('script')
+        raw_string = script_tag.text.strip().replace('window._sharedData =', '').replace(';', '')
+        return json.loads(raw_string)
+
+    def profile_page_metrics(self, profile_url):
+        results = {}
+        try:
+            response = self.__request_url(profile_url)
+            json_data = self.extract_json_data(response)
+            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']
+        except Exception as e:
+            raise e
+        else:
+            for key, value in metrics.items():
+                if key != 'edge_owner_to_timeline_media':
+                    if value and isinstance(value, dict):
+                        value = value['count']
+                        results[key] = value
+                    elif value:
+                        results[key] = value
+        return results
+
+    def profile_page_recent_posts(self, profile_url):
+        results = []
+        try:
+            response = self.__request_url(profile_url)
+            json_data = self.extract_json_data(response)
+            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']["edges"]
+        except Exception as e:
+            raise e
+        else:
+            for node in metrics:
+                node = node.get('node')
+                if node and isinstance(node, dict):
+                    results.append(node)
+        return results
+
+from pprint import pprint
+username = 'praddy_gr8'
+k = InstagramScraper()
+browser = webdriver.Chrome('/home/saumya/Documents/chromedriver')
+browser.get('https://www.instagram.com/'+username+'/?hl=en')
+
+results = k.profile_page_metrics('https://www.instagram.com/'+username+'/?hl=en')
+pprint(results)
+#To load the whole page
+PageLength = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    match=False
+        while(match==False):
+                lastCount = lenOfPage
+                time.sleep(3)
+                lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+                if lastCount==lenOfPage:
+                    match=True
+
+source_data = browser.page_source
+bs_data = BeautifulSoup(source_data)
